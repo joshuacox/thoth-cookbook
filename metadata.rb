d2eadc1a35779474c6129415c51ac0149b3c9235
Josh Cox <josh@webhosting.coop>
2014-11-18 15:25:09 -0600

    added hostsfile deps

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'uber thoth @ the mail by G'
 license          'All rights reserved'
 description      'Installs/Configures thoth env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+depends 'hostsfile'
+depends 'chef-solo-search'
